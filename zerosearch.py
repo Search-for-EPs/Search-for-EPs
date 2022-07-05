2b98e4f7b57beca5e2a6add395352f1b2d25242c
Patrick Egenlauf <p.egenlauf@gmx.de>
2022-07-05 14:50:53 +0200

    deleted unnecessary stuff and changed return value to kappe prediction of the ep

--- zerosearch.py
+++ zerosearch.py
@@ -4,22 +4,6 @@ import numpy as np
 from scipy import optimize
 
 
-def test_func(x):
-    return [x[1] ** 2 + x[0] + x[1],
-            x[0] + 3 * x[1] + 1]
-
-
-def test_jac(x):
-    return np.array([[1,
-                      1],
-                     [2 * x[1] + 1,
-                      3]])
-
-
 def zero_search(fun, guess):
-    return optimize.root(fun, np.array([guess.real, guess.imag]))
-
-
-if __name__ == '__main__':
-    sol = optimize.root(test_func, np.array([10, 10]))
-    print(sol.x)
+    sol = optimize.root(fun, np.array([guess.real, guess.imag]))
+    return np.array([complex(sol.x[0], sol.x[1])])
