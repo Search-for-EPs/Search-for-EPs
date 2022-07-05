aed70074acc13e3e6caaf05d3762bdec9180cec6
Patrick Egenlauf <p.egenlauf@gmx.de>
2022-07-05 14:49:07 +0200

    added model for sum of ev and changed all models to 2d model, can choose between rbf and matern kernel

--- gaussian_process_regression.py
+++ gaussian_process_regression.py
@@ -39,40 +39,11 @@ def gp_diff_angle(ev, phi):
     return m_re, m_im
 
 
-def gp_diff_kappa(ev, kappa):
-    ev_diff_re = ((ev[::, 0] - ev[::, 1]) ** 2).real.reshape(-1, 1)  # .astype(np.float64)
-    ev_diff_im = ((ev[::, 0] - ev[::, 1]) ** 2).imag.reshape(-1, 1)  # .astype(np.float64)
-    kappa_sep = np.column_stack([kappa.real, kappa.imag])  # .astype(np.float64)
-    k = gpflow.kernels.RBF()
-    kernel_ev = np.linalg.eigvals(k.K(kappa_sep))
-    m_re = gpflow.models.GPR(data=(kappa_sep, ev_diff_re), kernel=k, mean_function=None)
-    m_im = gpflow.models.GPR(data=(kappa_sep, ev_diff_im), kernel=k, mean_function=None)
-    opt = gpflow.optimizers.Scipy()
-    opt.minimize(m_re.training_loss, m_re.trainable_variables)
-    opt.minimize(m_im.training_loss, m_im.trainable_variables)
-    return m_re, m_im, kernel_ev
-
-
-def gp_diff_kappa_test(ev, kappa):
-    ev_diff_re = ((ev[::2, 0] - ev[::2, 1]) ** 2).real.reshape(-1, 1)  # .astype(np.float64)
-    ev_diff_im = ((ev[1::2, 0] - ev[1::2, 1]) ** 2).imag.reshape(-1, 1)  # .astype(np.float64)
-    kappa_sep1 = np.column_stack([kappa[::2].real, kappa[::2].imag])  # .astype(np.float64)
-    kappa_sep2 = np.column_stack([kappa[1::2].real, kappa[1::2].imag])
-    k = gpflow.kernels.RBF()
-    kernel_ev = np.linalg.eigvals(k.K(kappa_sep1))
-    m_re = gpflow.models.GPR(data=(kappa_sep1, ev_diff_re), kernel=k, mean_function=None)
-    m_im = gpflow.models.GPR(data=(kappa_sep2, ev_diff_im), kernel=k, mean_function=None)
-    opt = gpflow.optimizers.Scipy()
-    opt.minimize(m_re.training_loss, m_re.trainable_variables)
-    opt.minimize(m_im.training_loss, m_im.trainable_variables)
-    return m_re, m_im, kernel_ev
-
-
 def gp_2d_diff_kappa(ev, kappa):
     ev_diff_complex = ((ev[::, 0] - ev[::, 1]) ** 2)  # .astype(np.float64)
     ev_diff = np.column_stack([ev_diff_complex.real, ev_diff_complex.imag])
     kappa_sep = np.column_stack([kappa.real, kappa.imag])  # .astype(np.float64)
-    model, kernel_ev = gp_create_rbf_model(kappa_sep, ev_diff)
+    model, kernel_ev = gp_create_matern52_model(kappa_sep, ev_diff)
     return model, kernel_ev
 
 
@@ -80,12 +51,21 @@ def gp_2d_sum_kappa(ev, kappa):
     ev_sum_complex = (0.5 * (ev[::, 0] + ev[::, 1]))
     ev_sum = np.column_stack([ev_sum_complex.real, ev_sum_complex.imag])
     kappa_sep = np.column_stack([kappa.real, kappa.imag])
-    model, kernel_ev = gp_create_rbf_model(kappa_sep, ev_sum)
+    model, kernel_ev = gp_create_matern52_model(kappa_sep, ev_sum)
     return model, kernel_ev
 
 
 def gp_create_rbf_model(kappa, validation_data):
     k = gpflow.kernels.RBF()
+    kernel_ev = np.linalg.eigvals(k.K(kappa))
+    model = gpflow.models.GPR(data=(kappa, validation_data), kernel=k, mean_function=None)
+    opt = gpflow.optimizers.Scipy()
+    opt.minimize(model.training_loss, model.trainable_variables)
+    return model, kernel_ev
+
+
+def gp_create_matern52_model(kappa, validation_data):
+    k = gpflow.kernels.Matern52()
     kernel_ev = np.linalg.eigvals(k.K(kappa))
     model = gpflow.models.GPR(data=(kappa, validation_data), kernel=k, mean_function=None)
     opt = gpflow.optimizers.Scipy()
