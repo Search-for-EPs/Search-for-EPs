ba3f2b8ea530437c25f1777a9c862bc8cd50e8b1
Patrick Egenlauf <p.egenlauf@gmx.de>
2022-12-12 14:58:33 +0100

    changed jax.numpy to numpy to avoid device array errors

--- GPFlow_model_class.py
+++ GPFlow_model_class.py
@@ -1,6 +1,6 @@
-import jax.numpy as np
+import jax.numpy as jnp
 from jax import vmap
-import numpy
+import numpy as np
 import matrix
 import gpflow
 
@@ -43,11 +43,11 @@ class GPFlowModel:
             x_data : np.ndarray
                     x_data for which you want a new function value.
             """
-            xx, yy = numpy.meshgrid(x_data[0], x_data[1])
-            grid = numpy.array((xx.ravel(), yy.ravel())).T
+            xx, yy = np.meshgrid(x_data[0], x_data[1])
+            grid = np.array((xx.ravel(), yy.ravel())).T
             values, _ = self.model_diff.predict_f(grid)
 
-            return [numpy.float_(values.numpy().ravel()[0]), numpy.float_(values.numpy().ravel()[1])]
+            return [np.float_(values.numpy().ravel()[0]), np.float_(values.numpy().ravel()[1])]
 
         return _model_generator
 
@@ -65,10 +65,10 @@ class GPFlowModel:
             kappa : np.ndarray
                     kappa value for which you want the eigenvalues.
             """
-            symmatrix = matrix.matrix_one_close_re(numpy.array([complex(kappa[0], kappa[1])]))
+            symmatrix = matrix.matrix_one_close_re(np.array([complex(kappa[0], kappa[1])]))
             ev_new = matrix.eigenvalues(symmatrix)
-            xx, yy = numpy.meshgrid(kappa[0], kappa[1])
-            grid = numpy.array((xx.ravel(), yy.ravel())).T
+            xx, yy = np.meshgrid(kappa[0], kappa[1])
+            grid = np.array((xx.ravel(), yy.ravel())).T
             mean_diff, var_diff = self.model_diff.predict_f(grid)
             mean_sum, var_sum = self.model_sum.predict_f(grid)
             pairs_diff_all = np.empty(0)
@@ -76,9 +76,9 @@ class GPFlowModel:
             ev_1 = np.empty(0)
             ev_2 = np.empty(0)
             for i, val in enumerate(ev_new[0, ::]):
-                pairs_diff = vmap(lambda a, b: np.power((a - b), 2), in_axes=(None, 0), out_axes=0)(val,
+                pairs_diff = vmap(lambda a, b: jnp.power((a - b), 2), in_axes=(None, 0), out_axes=0)(val,
                                                                                                     ev_new[0, (i + 1):])
-                pairs_sum = vmap(lambda a, b: 0.5 * np.add(a, b), in_axes=(None, 0), out_axes=0)(val,
+                pairs_sum = vmap(lambda a, b: 0.5 * jnp.add(a, b), in_axes=(None, 0), out_axes=0)(val,
                                                                                                  ev_new[0, (i + 1):])
                 ev_1 = np.concatenate((ev_1, np.array([val for _ in range(len(ev_new[0, (i + 1):]))])))
                 ev_2 = np.concatenate((ev_2, ev_new[0, (i + 1):]))
@@ -98,6 +98,6 @@ class GPFlowModel:
             #                         marker=dict(color='red')))
             # fig.show()
             diff = ev_1[np.argmax(compatibility)] - ev_2[np.argmax(compatibility)]
-            return numpy.array([numpy.float_(diff.real), numpy.float_(diff.imag)])
+            return np.array([np.float_(diff.real), np.float_(diff.imag)])
 
         return _ep_evs
