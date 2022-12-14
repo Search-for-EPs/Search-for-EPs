de7ebbb79acc5eaa74aab6a6e2628168f46e7f9c
Patrick Egenlauf <p.egenlauf@gmx.de>
2022-12-14 13:57:55 +0100

    added package requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup
 
+requirements = [
+    "numpy>=1.22.4",
+    "gpflow>=2.6.3",
+    "jax>=0.3.13",
+    "pandas>=1.5.1",
+    "plotly>=5.11.0",
+    "matplotlib>=3.5.2",
+    "scipy>=1.8.1",
+    "setuptools>=59.6.0",
+]
 
 setup(name='searchep',
       version='0.1',
@@ -7,4 +17,5 @@ setup(name='searchep',
       author='Patrick Egenlauf',
       author_email='p.egenlauf@gmx.de',  # license='MIT',
       packages=['searchep'],
+      install_requires=requirements,
       zip_safe=False)
